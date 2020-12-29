2cf3da04a9b83b5a4055a88b2bdbc0bb9d462e23
alx-b <>
2020-12-29 21:22:49 +0100

    Models for podcast and episode.

---
+++ models.py
@@ -0,0 +1,23 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class PodcastIn:
+    title: str
+    url: str
+
+
+@dataclass
+class PodcastOut:
+    pk: int
+    title: str
+    url: str
+
+
+@dataclass
+class Episode:
+    podcast_id: int
+    number: int
+    title: str
+    url: str
+    date: str
