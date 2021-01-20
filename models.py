046a82cce7059df61beb7f28cb16193f181b8755
alx-b <>
2021-01-20 12:19:18 +0100

    Change an attribute on Episode dataclass.

--- models.py
+++ models.py
@@ -16,8 +16,11 @@ class PodcastOut:
 
 @dataclass
 class Episode:
-    # podcast_id: int
+    podcast_title: str
     number: int
     title: str
     url: str
     date: str
+
+    def __str__(self):
+        return f"{self.number}: {self.title} ({self.date})"
