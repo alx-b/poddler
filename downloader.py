f974cfbf298c08df7614a05ed75f1e589790f221
alx-b <>
2021-02-02 17:19:58 +0100

    Replace invalid chars for filename.

--- downloader.py
+++ downloader.py
@@ -8,13 +8,20 @@ def create_podcast_folder(folder_name):
     )
 
 
+def get_extension(text):
+    return text[text.rfind(".") : text.rfind(".") + 4]
+
+
 def download_file(episode):
     pod_title = episode.podcast_title.replace(" ", "_")
     ep_title = episode.title.replace(" ", "_")
+    ext = get_extension(episode.url)
+
     create_podcast_folder(pod_title)
+
     with httpx.stream("GET", episode.url) as stream:
         with open(
-            f"{Path.home()}/Music/podcasts/{pod_title}/{episode.number}._{ep_title}.mp3",
+            f"{Path.home()}/Music/podcasts/{pod_title}/{episode.number}._{ep_title}{ext}",
             "wb",
         ) as file:
             for chunk in stream.iter_bytes():
