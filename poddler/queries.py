2ae1f701695077170476e387331af5b474467833
alx-b <>
2021-05-09 11:52:28 +0200

    Docstrings on all functions in queries.

--- poddler/queries.py
+++ poddler/queries.py
@@ -1,19 +1,36 @@
 from typing import List, Tuple, Callable, Any
 import sqlite3
+
 # For the sake of shortening Type Annotation
 from sqlite3 import Connection, Cursor
 from models import PodcastIn
 
 
 def open_db(func: Callable) -> Any:
+    """Decorator to open connection to database.
+
+    Parameters:
+        func: Function/Callable
+    Returns:
+        Any: returns the result of whatever function
+    """
+
     def wrapper(*args, **kwargs):
         with sqlite3.connect("podcasts.db") as db:
             return func(db, *args, **kwargs)
+
     return wrapper
 
 
 @open_db
 def create_podcasts_table(db: Connection) -> None:
+    """If table podcasts doesn't exist, create it.
+
+    Parameters:
+        db: sqlite3.Connection (From decorator)
+    Returns:
+        None
+    """
     db.execute(
         """CREATE TABLE IF NOT EXISTS podcasts (
           id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -25,11 +42,13 @@ def create_podcasts_table(db: Connection) -> None:
 
 @open_db
 def insert_into_podcast_table(db: Connection, podcast: PodcastIn) -> None:
-    """Insert podcast model into database's podcasts table
+    """Insert podcast model into database's podcasts table.
 
     Parameters:
-        db: sqlite3.Connection
+        db: sqlite3.Connection (From decorator)
         podcast: models.PodcastIn
+    Returns:
+        None
     """
     db.cursor().execute(
         "INSERT INTO podcasts(title, url) VALUES (?, ?)",
@@ -40,6 +59,13 @@ def insert_into_podcast_table(db: Connection, podcast: PodcastIn) -> None:
 
 @open_db
 def get_all_podcasts(db: Connection) -> List[Tuple]:
+    """Return a list of podcasts data from the database.
+
+    Parameters:
+        db: sqlite3.Connection (From decorator)
+    Returns:
+        List[Tuple[int, str, str]]
+    """
     cursor = db.cursor()
     cursor.execute("SELECT * FROM podcasts")
     return cursor.fetchall()
@@ -47,6 +73,14 @@ def get_all_podcasts(db: Connection) -> List[Tuple]:
 
 @open_db
 def get_a_podcast_by_title(db: Connection, title: str) -> Tuple[int, str, str]:
+    """Return a tuple of a podcast data by its title from the database.
+
+    Parameters:
+        db: sqlite3.Connection (From decorator)
+        title: str
+    Returns:
+        Tuple[int, str, str]
+    """
     cursor = db.cursor()
     cursor.execute("SELECT * FROM podcasts WHERE title=?", (title,))
     return cursor.fetchone()
@@ -54,9 +88,16 @@ def get_a_podcast_by_title(db: Connection, title: str) -> Tuple[int, str, str]:
 
 @open_db
 def delete_a_podcast_by_title(db: Connection, title: str) -> None:
+    """Delete a podcast by its title from the database.
+
+    Parameters:
+        db: sqlite3.Connection (From decorator)
+        title: str
+    Returns:
+        None
+    """
     db.cursor().execute("DELETE FROM podcasts WHERE title=?", (title,))
     db.commit()
 
 
-if __name__ == "__main__":
-    create_podcasts_table()
+create_podcasts_table()
